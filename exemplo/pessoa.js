19acbf451c2b3b11a97d220a6fe64899fd4cf10a
Jussimar Nascimento Leal <jussimar@jussimarleal.com.br>
2020-02-08 01:06:44 +0000

    criando classe

---
+++ exemplo/pessoa.js
@@ -0,0 +1,28 @@
+var empresa;
+(function (empresa) {
+    var Pessoa = /** @class */ (function () {
+        function Pessoa(nome, cpf) {
+            this.cpf = cpf;
+            this.nome = nome;
+        }
+        Pessoa.prototype.getCpf = function () {
+            return this.cpf;
+        };
+        //mostra o valor
+        Pessoa.prototype.getNome = function () {
+            return this.nome;
+        };
+        //altera o valor
+        Pessoa.prototype.setNome = function (nome) {
+            this.nome = nome;
+        };
+        Pessoa.prototype.getIdade = function () {
+            return this.idade;
+        };
+        //altera o valor
+        Pessoa.prototype.setIdade = function (idade) {
+            this.idade = idade;
+        };
+        return Pessoa;
+    }());
+})(empresa || (empresa = {}));
---
+++ exemplo/pessoa.ts
@@ -0,0 +1,36 @@
+namespace empresa{
+    class Pessoa{
+        //informações da classe
+        private nome:string;
+        private cpf:string;
+        private idade:number|undefined;
+
+        constructor(nome:string, cpf:string){
+            this.cpf = cpf;
+            this.nome = nome;
+        }
+
+        getCpf():string{
+            return this.cpf;
+        }
+
+        //mostra o valor
+        getNome():string|undefined{
+            return this.nome;
+        }
+
+        //altera o valor
+        setNome(nome:string):void{
+            this.nome = nome;
+        }
+        
+        getIdade():number|undefined{
+            return this.idade;
+        }
+
+        //altera o valor
+        setIdade(idade:number):void{
+            this.idade = idade;
+        }
+    }
+}
